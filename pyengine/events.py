8b34f492c79cf97ea55c0e04701eb083b5125c08
SmoothTurtle872 <whatleyfamily1@gmail.com>
2025-03-14 12:34:17 +0930

    Documented the Event class

--- pyengine/events.py
+++ pyengine/events.py
@@ -1,4 +1,11 @@
 class Event:
+    """
+    A representation of an event.
+    :param event_type: The event type of this event : default 0
+    :param data: The data of this event : default None
+
+    When sent by the ``APP.sendEvent()`` function where ``APP`` is the instance of the App class, a default value for the event data is set to a tuple containing the event timestamp and any additional data which defaults to None
+    """
     type: int
     data: tuple | None
     def __init__(self, event_type: int = 0, data: tuple | None = None):
